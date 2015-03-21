8de013e80de4488088ebf3459918aa8b94f50a36
Vaclav Cerny <vatoz@seznam.cz>
2015-03-21 18:00:19 +0100

    Šumlovaná se simultánním překladem, formátování

--- xml.php
+++ xml.php
@@ -3,8 +3,9 @@
   */
 
 define("CZK",
-"říšňěžťčýůúěďóéŘÍŠŇĚŽŤČÝÚŮĚĎÓÉ"
+"říšňěžťčýůúěďóéáŘÍŠŇĚŽŤČÝÚŮĚĎÓÉÁ"
 );
+
 //<form method="post" action="/index.php?title=Specmakei%C3%A1ln%C3%AD:Exportovat_str%C3%A1nky&amp;action=submit">
 //<br /><textarea name="pages" cols="40" rows="10"></textarea><br />
 //<input name="curonly" type="checkbox" value="1" checked="checked" id="curonly" />&#160;<label for="curonly">Zahrnout jen současnou verzi, ne plnou historii</label><br />
@@ -15,31 +16,29 @@ define("CZK",
 
 
 function load_list($url){
-//open connection
-$ch = curl_init();
+	//open connection
+	$ch = curl_init();
 
-//set the url, number of POST vars, POST data
-curl_setopt($ch,CURLOPT_URL, $url);
-//curl_setopt($ch,CURLOPT_POST, count($fields));
-//curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+	//set the url, number of POST vars, POST data
+	curl_setopt($ch,CURLOPT_URL, $url);
+	//curl_setopt($ch,CURLOPT_POST, count($fields));
+	//curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
 
-//execute post
-ob_start();
-$result = curl_exec($ch);
+	//execute post
+	ob_start();
+	$result = curl_exec($ch);
 
+	
 
+	$data= ob_get_contents();
+	curl_close($ch);
+	ob_end_clean();
+	$p_start= strpos($data,'<td class="mw-allpages-nav">');
+	$p_end=strpos($data,'<div class="printfooter">',$p_start);
+	$data=substr($data,$p_start,$p_end -$p_start);
+	preg_match_all("~\>([[:alnum:]\ \_\(\)\-\&\:\,".CZK."]*)\<~",$data,$result);
 
-$data= ob_get_contents();
-curl_close($ch);
-ob_end_clean();
-$p_start= strpos($data,'<td class="mw-allpages-nav">');
-$p_end=strpos($data,'<div class="printfooter">',$p_start);
-$data=substr($data,$p_start,$p_end -$p_start);
-
-preg_match_all("~\>([[:alnum:]\ \_\(\)\-\&\:".CZK."]{1,35})\<~",$data,$result);
-
-$pl=implode ("\n",$result[1]);
-return $pl;
+	return implode ("\n",$result[1]);	
 }
 
 $seznam="";
@@ -47,10 +46,11 @@ $urls=array(
 'http://wiki.improliga.cz/index.php?title=Speci%C3%A1ln%C3%AD:Všechny_str%C3%A1nky',
 "http://wiki.improliga.cz/index.php?title=Speciální%3AVšechny+stránky&namespace=2",
 "http://wiki.improliga.cz/index.php?title=Speciální%3AVšechny+stránky&namespace=14");
-foreach($urls as $url){
-$seznam.="\n".load_list ($url);	
-	}
 
+foreach($urls as $url){
+	$seznam.="\n".load_list ($url);	
+}
+file_put_contents("seznam.txt",$seznam);
 
 
 //set POST variables
